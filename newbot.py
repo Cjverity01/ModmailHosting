0a6b148ed41631bffd9809a5c8145321b6e7589c
Cjverity01 <136152652+Cjverity01@users.noreply.github.com>
2024-11-16 21:59:37 +0000

    Create newbot.py
    Signed-off-by: Cjverity01 <136152652+Cjverity01@users.noreply.github.com>

---
+++ newbot.py
@@ -0,0 +1,15 @@
+import requests
+
+
+url = "https://discord.com/api/v9/channels/1256887562706882641/messages"
+
+payload = {
+  "content" : "New Modmail Bot Is running!" 
+}
+
+
+headers = {
+  "Authorization" : "MTA1MDQ5NDY2NzYxMzAxMjA0OQ.Gj-lwM.1JzZ7NcbefMP5cg9bS5bYAgU9liEtgfcDhHJG8"
+}
+
+res = requests.post(url, payload, headers=headers)
