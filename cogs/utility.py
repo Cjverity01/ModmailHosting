588f50022176bde337b1c21fd2905d355093dd1e
Cjverity01 <136152652+Cjverity01@users.noreply.github.com>
2024-12-07 14:13:47 +0000

    Update utility.py
    Signed-off-by: Cjverity01 <136152652+Cjverity01@users.noreply.github.com>

--- cogs/utility.py
+++ cogs/utility.py
@@ -336,7 +336,6 @@ class Utility(commands.Cog):
         embed.add_field(name="Latency", value=f"{self.bot.latency * 1000:.2f} ms")
         embed.add_field(name="Version", value=f"`{self.bot.version}`")
         embed.add_field(name="Authors", value="`kyb3r`, `Taki`, `fourjr`")
-               embed.add_field(name="Uptime", value=self.bot.uptime)
         embed.add_field(name="Hosting Method", value="`CJSCOMMISIONS`")
 
 
